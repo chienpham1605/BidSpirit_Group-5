3d5edc3a5ab98386393559b77e4eb4c44686a828
chienpham1605 <107631195+chienpham1605@users.noreply.github.com>
2022-08-11 10:22:08 +0700

    change

--- ctrller.js
+++ ctrller.js
@@ -144,6 +144,14 @@ var searchFilter = () => {
       cards[i].classList.add("d-none");
     }
   }
-};
+}
+// visit count
+var n = localStorage.getItem('on_load_counter');
+    if (n === null) {
+        n = 0;
+    }
+    n++;
+    localStorage.setItem("on_load_counter", n);
+    document.getElementById('CounterVisitor').innerHTML = n;
 
 
