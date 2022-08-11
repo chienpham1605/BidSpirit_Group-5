aa50ae858f43620826ff551769827f558d16927c
chienpham1605 <107631195+chienpham1605@users.noreply.github.com>
2022-08-11 10:22:31 +0700

    change

--- ctrller.js
+++ ctrller.js
@@ -145,13 +145,6 @@ var searchFilter = () => {
     }
   }
 }
-// visit count
-var n = localStorage.getItem('on_load_counter');
-    if (n === null) {
-        n = 0;
-    }
-    n++;
-    localStorage.setItem("on_load_counter", n);
-    document.getElementById('CounterVisitor').innerHTML = n;
+
 
 
